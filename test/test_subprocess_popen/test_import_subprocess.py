3acf1cff1e73baba7b5b5945f2ad1939d92f0cc4
Bryce Cai <29902109+bcai2@users.noreply.github.com>
2022-09-07 13:48:36 -0700

    feat: run subprocess without `shell=True` (#3)
    * feat: run subprocess without shell=True
    
    * fix/multiple-args
    
    * fix: lint
    
    Co-authored-by: Noah Lebovic <security@trytoolchest.com>

--- test/test_subprocess_popen/test_import_subprocess.py
+++ test/test_subprocess_popen/test_import_subprocess.py
@@ -84,3 +84,24 @@ def test_subfunction(number, **kwargs):
 
 def subfunction(name='World'):
     print(f"{name}!")
+
+
+@pytest.mark.unit
+@base_test_decorator
+@lug.run(image=BASE_TEST_IMAGE)
+def test_without_shell(number, **kwargs):
+    """Tests behavior when shell=True is not specified and a list is passed in as args."""
+    run_value = subprocess.Popen(["/bin/ls", "-l"])
+    print("return code:", run_value.wait())
+    return number
+
+
+@pytest.mark.unit
+@base_test_decorator
+@lug.run(image=BASE_TEST_IMAGE)
+def test_check_output_redirects(number, **kwargs):
+    """Tests behavior when shell=True is not specified and a list is passed in as args."""
+    p = subprocess.Popen(["ls", "/"], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    assert out == b'bin\ndev\netc\nhome\nlib\nlug\nmedia\nmnt\nopt\nproc\nroot\nrun\nsbin\nsrv\nsys\ntmp\nusr\nvar\n'
+    return number
