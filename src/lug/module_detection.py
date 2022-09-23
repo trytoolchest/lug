c3b803d6be05b2fce6d541ff38aff8264eb5bfa2
Noah Lebovic <security@trytoolchest.com>
2022-09-23 11:41:28 -0700

    fix: add comments to get_registered_module_imports

--- src/lug/module_detection.py
+++ src/lug/module_detection.py
@@ -15,6 +15,18 @@ def parse_ast_for_file(python_file_path):
 
 
 def get_registered_module_imports(modules_to_register, modules_to_skip):
+    """
+    Perform a deep search for module dependencies, by parsing the AST and looking for all imports.
+
+    All imports in the module are parsed, even if they aren't used. Practically, this means we're catching
+    more dependencies than needed within a module.
+
+    Modules that this function finds are not searched for _their_ dependencies. For a truly deep search, this would need
+    to be modified to search through found modules and their dependencies.
+
+    Only modules that are already loaded within `sys.modules` or the Lugged function are registered for pickling.
+    """
+    # Find all sys level registered modules that aren't builtins
     filtered_sys_modules = []
     for module_name, module_value in sys.modules.items():
         is_builtin_module = module_name in sys.builtin_module_names
@@ -22,17 +34,22 @@ def get_registered_module_imports(modules_to_register, modules_to_skip):
         if not (is_builtin_module or is_stdlib_module):
             filtered_sys_modules.append(module_name)
 
+    # Find all imported modules within dependencies
     deep_modules = set()
     for module in modules_to_register:
         all_ast_nodes = []
 
+        # Some modules are single-file modules, or otherwise don't have a root directory
         if not hasattr(module, "__path__"):
+            # We're not supporting modules that have neither a directory or file at their root
             if not hasattr(module, "__file__"):
                 continue
+            # If the module only has one file, we only need to parse that file
             all_ast_nodes = parse_ast_for_file(
                 python_file_path=getattr(module, "__file__")
             )
         else:
+            # If the module has a directory root, we'll need to parse all .py files in the directory
             module_path = getattr(module, "__path__")
             if len(module_path) == 1:  # Most packaged modules have a source directory
                 python_files = glob.glob(os.path.join(module_path[0], "**.py"))
@@ -46,6 +63,10 @@ def get_registered_module_imports(modules_to_register, modules_to_skip):
             for python_file_path in python_files:
                 all_ast_nodes += parse_ast_for_file(python_file_path)
 
+        # Now that we have the AST for all Python files in the module, we need to find the imports
+        # There are two types of imports: direct imports and renamed imports. E.g.:
+        # Direct import: import subprocess
+        # Renamed import from subprocess import run
         ast_direct_imports = []
         ast_renamed_imports = []
         for node in all_ast_nodes:
@@ -53,16 +74,19 @@ def get_registered_module_imports(modules_to_register, modules_to_skip):
                 ast_direct_imports.append(node)
             if isinstance(node, ast.ImportFrom):
                 ast_renamed_imports.append(node)
+
+        # Both types of imports have different structures.
+        # They're parsed differently, but added to the same list of imports once we find the module name.
         for renamed_import in ast_renamed_imports:
             module_name = renamed_import.module
             if module_name in filtered_sys_modules and module_name not in modules_to_skip:
-                if "_pytest" in module.__name__ or "url" in module.__name__:
+                if "_pytest" in module.__name__:
                     continue
                 deep_modules.add(sys.modules[module_name])
         for direct_import in ast_direct_imports:
             module_name = direct_import.names[0].name
             if module_name in filtered_sys_modules and module_name not in modules_to_skip:
-                if "_pytest" in module.__name__ or "url" in module.__name__:
+                if "_pytest" in module.__name__:
                     continue
                 deep_modules.add(sys.modules[module_name])
     return deep_modules
