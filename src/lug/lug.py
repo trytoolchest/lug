e4fc0a5fdaca827701f4c7477afb5cd123ebfcd0
Noah Lebovic <security@trytoolchest.com>
2023-01-26 14:09:45 -0600

    fix: default to aws

--- src/lug/lug.py
+++ src/lug/lug.py
@@ -479,7 +479,7 @@ def execute_local(mount, client, user_docker, func, args, kwargs, docker_shell_l
 def run(image=None, mount=os.getcwd(), tmp_dir=tempfile.gettempdir(), docker_shell_location="/bin/sh", remote=False,
         remote_inputs=None, remote_output_directory=None, toolchest_key=None, remote_instance_type=None,
         volume_size=None, serialize_dependencies=True, command_line_args="", streaming_enabled=True,
-        redirect_shell=True, provider=None):
+        redirect_shell=True, provider="aws"):
     def decorator_lug(func):
         @functools.wraps(func)
         def inner(*args, **kwargs):
