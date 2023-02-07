9287a81da48a8081ab23f210380499121d05ee50
Noah Lebovic <security@trytoolchest.com>
2023-02-07 15:05:03 -0600

    feat: automatically capture and combine stdout and stderr for sidecar_shell

--- src/lug/shell.py
+++ src/lug/shell.py
@@ -5,5 +5,12 @@ def sidecar_shell(command, **kwargs):
     """
     This function is redirected to the Lug sidecar container. The first arg must remain the shell command.
     """
-    result = subprocess.run(command, text=True, shell=True, **kwargs)
+    result = subprocess.run(
+        command,
+        text=True,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        **kwargs,
+    )
     return result
